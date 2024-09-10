64a7ebd7f9f766a4244833956a5a1c3c0f46cd9a
Neshama1 <107024700+Neshama1@users.noreply.github.com>
2024-09-09 19:54:53 -0600

    Delete build directory

--- build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mike/Devel/QML/MauiKit/novaiweb/src
-# Build directory: /home/mike/Devel/QML/MauiKit/novaiweb/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
